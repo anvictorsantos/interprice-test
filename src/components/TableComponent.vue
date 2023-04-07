f32c2c65a5b99b324beecc724f3754bf67a2246d
Antonio Pontes <anvictorsantos@gmail.com>
2023-04-07 19:56:01 -0300

    feat: table component
    WIP: interactive table rows from data.json file

---
+++ src/components/TableComponent.vue
@@ -0,0 +1,54 @@
+<template>
+  <section>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <table class="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">DATE SENT</th>
+                <th scope="col">COMPANY</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="item in items" :key="item.Id">
+                <td scope="row">{{ item.DateSent }}</td>
+                <td scope="row">{{ item.Company }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'TableComponent',
+  props: ['data'],
+  data () {
+    return {
+      items: this.data
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TableComponent :data="items"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import data from '../../data.json'
+import TableComponent from '@/components/TableComponent.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    TableComponent
+  },
+  data: () => {
+    return {
+      items: data.Items
+    }
   }
 }
 </script>
